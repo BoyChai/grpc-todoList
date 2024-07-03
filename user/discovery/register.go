2dcc55f107d98576a977f58ae9b52c2ec543a949
BoyChai <1972567225@qq.com>
2024-07-03 20:16:25 +0800

    feat: etcd module 2

--- user/discovery/register.go
+++ user/discovery/register.go
@@ -1,6 +1,13 @@
 package discovery
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
 	"github.com/sirupsen/logrus"
 	"go.etcd.io/etcd/clientv3"
 )
@@ -14,6 +21,94 @@ type Register struct {
 
 	srvInfo Server
 	SrvTTL  int64
-	cki     *clientv3.Client
+	cli     *clientv3.Client
 	logger  *logrus.Logger
+}
+
+// NewRegister 基于ETCD创建一个register
+func NewRegister(etcdAddrs []string, logger logrus.Logger) *Register {
+	return &Register{
+		EtcdAddres:  etcdAddrs,
+		DialTimeout: 3,
+		logger:      &logger,
+	}
+}
+
+func (r *Register) Register(srvInfo Server, ttl int64) (chan<- struct{}, error) {
+	var err error
+	if strings.Split(srvInfo.Addr, ":")[0] == "" {
+		return nil, errors.New("invalid ip address")
+	}
+	// 初始化
+	if r.cli, err = clientv3.New(clientv3.Config{
+		Endpoints:   r.EtcdAddres,
+		DialTimeout: time.Duration(r.DialTimeout) * time.Second,
+	}); err != nil {
+		return nil, err
+	}
+
+	r.srvInfo = srvInfo
+	r.SrvTTL = ttl
+	if err = r.register(); err != nil {
+		return nil, err
+	}
+
+	r.closeCh = make(chan struct{})
+	go r.keepAlive()
+	return r.closeCh, nil
+}
+
+func (r *Register) register() error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(r.DialTimeout)*time.Second)
+	defer cancel()
+	leaseResp, err := r.cli.Grant(ctx, r.SrvTTL)
+	if err != nil {
+		return err
+	}
+	r.leasesID = leaseResp.ID
+
+	if r.keepAliveCh, err = r.cli.KeepAlive(context.Background(), r.leasesID); err != nil {
+		return err
+	}
+	data, err := json.Marshal(r.srvInfo)
+	if err != nil {
+		return err
+	}
+	_, err = r.cli.Put(context.Background(), buildRegisterPath(r.srvInfo), string(data), clientv3.WithLease(r.leasesID))
+
+	return err
+}
+
+func (r *Register) keepAlive() error {
+
+	ticker := time.NewTicker(time.Duration(r.SrvTTL) * time.Second)
+	for {
+		select {
+		case <-r.closeCh:
+			if err := r.unregister(); err != nil {
+				fmt.Println("unregister failed error", err)
+			}
+			if _, err := r.cli.Revoke(context.Background(), r.leasesID); err != nil {
+				fmt.Println("revoke fail")
+			}
+		case res := <-r.keepAliveCh:
+			if res == nil {
+				if err := r.register(); err != nil {
+					fmt.Println("register err")
+				}
+			}
+		case <-ticker.C:
+			if r.keepAliveCh == nil {
+				if err := r.register(); err != nil {
+					fmt.Println("register err")
+				}
+			}
+		}
+
+	}
+}
+
+func (r *Register) unregister() error {
+	_, err := r.cli.Delete(context.Background(), buildRegisterPath(r.srvInfo))
+	return err
 }
