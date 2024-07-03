09de31f69b57a9fab45622c047a8aa74af10d969
BoyChai <1972567225@qq.com>
2024-07-03 17:00:30 +0800

    feat: add user dao layer

--- user/internal/repository/user.go
+++ user/internal/repository/user.go
@@ -1,8 +1,80 @@
 package repository
 
+import (
+	"errors"
+	"user/internal/service"
+
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
 type User struct {
 	UserId         uint   `gorm: "primarykey`
 	UserName       string `gorm:"unique"`
 	NickName       string
 	PasswordDigest string
+}
+
+const (
+	PasswordCode = 12 //密码加密难度
+)
+
+// 检查用户是否注册
+func (user *User) CheckUserExits(req *service.UserRequest) bool {
+	if err := DB.Where("user_name=?", req.UserName).First(&user).Error; err == gorm.ErrRecordNotFound {
+		return false
+	}
+	return true
+}
+
+// 获取用户信息
+func (user *User) ShowUserInfo(req *service.UserRequest) (err error) {
+	if exist := user.CheckUserExits(req); exist {
+		return nil
+	}
+	return errors.New("UserName Not Exits")
+}
+
+// 密码加密
+func (user *User) SetPassword(password string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), PasswordCode)
+	if err != nil {
+		return err
+	}
+	user.PasswordDigest = string(bytes)
+	return nil
+}
+
+// 用户创建
+func (*User) UserCreate(req *service.UserRequest) error {
+	var count int64
+	DB.Where("user_name=?", req.UserName).Count(&count)
+
+	if count != 0 {
+		return errors.New("UserName Exist")
+	}
+	user := User{
+		UserName: req.UserName,
+		NickName: req.NickName,
+	}
+	// 密码的加密
+	_ = user.SetPassword(req.Password)
+	err := DB.Create(&user).Error
+	return err
+}
+
+// CheckPassword 检验密码
+func (user *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(user.PasswordDigest), []byte(password))
+	return err == nil
+}
+
+// BuildUser 序列化User
+func BuildUser(item User) *service.UserModel {
+	userModel := service.UserModel{
+		UserID:   uint32(item.UserId),
+		UserName: item.UserName,
+		NickName: item.NickName,
+	}
+	return &userModel
 }
